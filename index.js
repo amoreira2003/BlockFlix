6c7fbb47db2f982cf6815953a0f4ba3a6bda6f5f
Alexandre Moreira <alexrmoreira777@gmail.com>
2023-04-23 09:07:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express")
 const app = express();
 const port = 3000;
 
-
 app.use("/static",express.static(__dirname + "/public"))
 
+app.get('/', (req, res) => {
+    res.send("Hello World!");
+});
 
 app.listen(port, () => {
     console.log("Hosted on port" + port)
