24a7cfd5593001d2ff3f4e8336b9865faa588890
Alexandre Moreira <alexrmoreira777@gmail.com>
2023-04-23 08:18:57 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app = express();
+const port = 3000;
+
+
+app.use("/static",express.static(__dirname + "/public"))
+
+
+app.listen(port, () => {
+    console.log("Hosted on port " + port)
+})
