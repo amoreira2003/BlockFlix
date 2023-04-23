a4183526fcc6292ee286ffba23f0dac90c0b8a32
Alexandre Moreira <alexrmoreira777@gmail.com>
2023-04-23 09:50:54 -0300

    Test with streaming

---
+++ a/index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express();
+const port = 3000;
+
+app.use("/static",express.static(__dirname + "/public"))
+
+app.get('/', (req, res) => {
+    res.send("Hello World!");
+    console.log("Pong")
+});
+
+app.listen(port, () => {
+    console.log("Hosted on port" + port)
+})
